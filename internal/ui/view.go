a00b7c48c9b0ef612180b6ccc56cea2eb08a333d
njaal0 <njalnordal@gmail.com>
2026-01-08 11:23:56 +0100

    ...

--- internal/ui/view.go
+++ internal/ui/view.go
@@ -38,7 +38,7 @@ func (m *Model) View() string {
 		b.WriteString("\n")
 	}
 
-	b.WriteString("\n↑↓ navigate   → open   ← back   q quit")
+	b.WriteString("\n↑↓: navigate   →: open   ←: back   c: cancel cd after quit   q: quit")
 
 	return b.String()
 }
